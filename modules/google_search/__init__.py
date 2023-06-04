b2be91d0bd661d3fa78c87611afa8bb8c5e3b8f3
onions41 <kblee89@outlook.com>
2023-06-04 13:58:00 -0700

    Iterates over pages via next and yields results

--- modules/google_search/__init__.py
+++ modules/google_search/__init__.py
@@ -1,10 +1,9 @@
 import re, logging
 import requests
+from urllib.parse import urlparse, parse_qs
 from time import sleep
 from bs4 import BeautifulSoup
-from common.user_agents import get_useragent
-
-logger = logging.getLogger(__name__)
+from ..common.user_agents import get_useragent
 
 
 def search(search_string):
@@ -17,42 +16,63 @@ def search(search_string):
     params = {"q": search_string, "num": "25"}
     res = requests.get(base_url, params=params, headers=headers)
 
-    # if res.status_code == not ok
-    # should log the problem
-    logger.info("Holy shat******")
+    # Loops until there are no more next pages.
+    # Not using recursion, no need to crowd the stack.
+    page = 1
+    while True:
+        if res.status_code != 200:
+            logging.info("Status code not 200 for " + search_string)
+            return "status code not 200"
 
-    if res.text:
-        soup = BeautifulSoup(res.text)
+        if res.text:
+            soup = BeautifulSoup(res.text, "html.parser")
 
-    # Check if there are no results found
-    pattern = re.compile("your search.+did not match any documents", flags=re.I)
-    for tag in soup.select("div.card-section"):
-        if pattern.search(tag.find("p").text):
-            return "no results"  # This is the value of the StopIteration exception
+        # Check if there are no results found
+        pattern = re.compile("your search.+did not match any documents", flags=re.I)
+        for tag in soup.select("div.card-section"):
+            if pattern.search(tag.find("p").text):
+                logging.info("No results found for " + search_string)
+                return "no results"
 
-    # Yield search results
-    result_block = soup.find_all("div", attrs={"class": "g"})
-    for result in result_block:
-        # Find link, title, description
-        link = result.find("a", href=True)
-        title = result.find("h3")
-        description_box = result.find("div", {"style": "-webkit-line-clamp:2"})
-        if description_box:
-            description = description_box.text
-            if link and title and description:
-                yield {
-                    "link_url": link["href"],
-                    "link_title": title.text,
-                    "link_description": description,
-                }
+        # Yield search results
+        result_block = soup.find_all("div", attrs={"class": "g"})
+        for result in result_block:
+            # Find link, title, description
+            link = result.find("a", href=True)
+            title = result.find("h3")
+            description_box = result.find("div", {"style": "-webkit-line-clamp:2"})
+            if description_box:
+                description = description_box.text
+                if link and title and description:
+                    yield {
+                        "link_url": link["href"],
+                        "link_title": title.text,
+                        "link_description": description,
+                    }
 
-    # Check if there is a next page
-    pattern = re.compile("next$", flags=re.I)
-    for tag in soup.find_all(
-        lambda tag: tag.name == "div"
-        and tag.has_attr("role")
-        and tag["role"] == "navigation"
-    ):
-        if pattern.search(tag.text):
-            yield
-            # navigate to next page and rescurse
+        logging.info("Searched " + search_string + ". Result yielded for page " + str(page))
+        # Check if there is a next page
+        pattern = re.compile("next$", flags=re.I)
+        div_tag = soup.find(
+            lambda tag: tag.name == "div"
+            and tag.has_attr("role")
+            and tag["role"] == "navigation"
+            and pattern.search(tag.text)
+        )
+
+        # Means there is no next page
+        if div_tag is None:
+            break
+
+        next_link = div_tag.find(
+            lambda tag: tag.name == "a" and pattern.search(tag.text)
+        )
+
+        # Parse out the next_link tag's href into params to use requests
+        params = {}
+        query_dict = parse_qs(urlparse(next_link["href"]).query)
+        for k, v in query_dict.items():
+            params[k] = v[0]
+
+        sleep(5)  # Sleep 5 seconds. Not in a hurry, don't want to IP get banned.
+        res = requests.get(base_url, params=params, headers=headers)
