7595b7693365a05de4137a6adc0dacc4ab8d164a
onions41 <kblee89@outlook.com>
2023-06-05 21:29:41 -0700

    Yields resources as byte string from links in page

--- modules/get_page_links.py
+++ modules/get_page_links.py
@@ -1,7 +1,6 @@
-import sys
 import re, logging
 import requests
-from urllib.parse import urlparse, parse_qs, urlencode
+from urllib.parse import urljoin
 from time import sleep
 from bs4 import BeautifulSoup
 from .common.user_agents import get_useragent
@@ -10,4 +9,27 @@ from .common.user_agents import get_useragent
 # href is a full url string to the page that contains links to the resources
 # yields the bytestring data of each resource
 def get_page_links(href):
-    print(href)
+    headers = {"User-Agents": get_useragent()}
+    res = requests.get(href, headers=headers)
+
+    if res.status_code != 200:
+        logging.info("Status code not 200 for " + href)
+        return "status code not 200"
+
+    soup = BeautifulSoup(res.text, "html.parser")
+    pattern = re.compile("min\.pdf$", flags=re.I)
+    links = soup.find_all("a", href=pattern)
+
+    if not link:
+        logging.info("No resources found in " + href)
+        return "no resources found"
+
+    for link in links:
+        sleep(5)
+        resource_url = urljoin(href, link["href"])
+        res = requests.get(resource_url, headers=headers)
+        if res.status_code == 200:
+            logging.info("Got " + resource_url)
+            yield res.content
+        else:
+            logging.info("Status code not 200 for " + resource_url)
